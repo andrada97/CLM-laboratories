d47f99db940d524846f2253bcf0f3737d9e908aa
Tapuc Andrada <andradaa.didaa@yahoo.com>
2019-10-10 16:15:08 +0300

    Pana la 4 si putin din 5 :))

--- Lexicografie/laborator2/laborator2.py
+++ Lexicografie/laborator2/laborator2.py
@@ -9,11 +9,42 @@ listOfName = ["a priori", "San Francisco"]
 def readCorpusFromFile(corpusName):
     file = open(corpusName,"r") 
     return file.read()
+
+#Task 1
 def tokenizeText(text):
     return casual_tokenize(text)
 
+#Task 2
+def checkIpAddresses(token):
+    check = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",token)
+    if(check):
+        return True
+    return False
+
+def concatIpAddress(listOfTokens):
+    newList = []
+    index = 0
+    while index < len(listOfTokens) :
+        if index < len(listOfTokens)-2 and (checkIpAddresses(listOfTokens[index] + listOfTokens[index + 1])):
+            newList.append(listOfTokens[index] + listOfTokens[index + 1])
+            index = index + 1
+        else:
+            if index < len(listOfTokens)-3 and (checkIpAddresses(listOfTokens[index] + listOfTokens[index + 1] + listOfTokens[index + 2])):
+                newList.append(listOfTokens[index] + listOfTokens[index + 1] + listOfTokens[index + 2])
+                index = index + 2
+            else:
+                newList.append(listOfTokens[index])
+            index = index + 1
+    return newList
+
+
+#Task 3       
 def checkStartWith(token):
+    #Verificare daca incepe cu litera mare
     return re.match(startWithBigLettter, token) or token in listOfAbrv
+  
+def checkForAbreviation(listOfTokens, index):
+    return checkStartWith(listOfTokens[index]) and listOfTokens[index+1] == '.' and checkStartWith(listOfTokens[index+2])
 
 def concatNames(listOfTokens):
     newList = []
@@ -26,10 +57,6 @@ def concatNames(listOfTokens):
             newList.append(listOfTokens[index])
         index = index +1
     return newList        
-            
-def checkForAbreviation(listOfTokens, index):
-    return checkStartWith(listOfTokens[index]) and listOfTokens[index+1] == '.' and checkStartWith(listOfTokens[index+2])
-
 
 
 ####
@@ -40,6 +67,7 @@ def printList(listToPrint):
 if __name__ == "__main__":
     
     listOfTokens = tokenizeText(readCorpusFromFile(corpusName))
+    listOfTokens = concatIpAddress(listOfTokens)
     listOfTokens = concatNames(listOfTokens)
     printList(listOfTokens)
     
